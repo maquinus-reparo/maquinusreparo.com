5336e0ef19c228542d010f498a7d15ba5bfa799f
Eduardo Flores <edfloreshz@gmail.com>
2023-02-18 15:42:24 -0700

    New cards

--- js/index.js
+++ js/index.js
@@ -6,13 +6,15 @@ getServices().then(services => {
     let html = '';
     services.forEach(service => {
         const card = `
-            <div class="card">
-                <div class="card-header">
-                    ${service.spell}
-                </div>
-                <img src="${service.icon}" class="card-icon" />
-                <div class="card-body">
-                    ${service.description}
+            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-4 mt-3 mb-3">
+                <div class="card">
+                    <div class="card-header">
+                        ${service.spell}
+                    </div>
+                    <div class="card-body">
+                        <img src="${service.icon}" class="card-icon" />
+                        <p>${service.description}</p>
+                    </div>
                 </div>
             </div>
         `;
